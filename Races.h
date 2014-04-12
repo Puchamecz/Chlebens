1a1023d848593c765f086a639a82ac219bd929ae
Azorixz <koscielak.bartosz@gmail.com>
2014-04-12 17:30:33 +0200

    5

--- Races.h
+++ Races.h
@@ -1,5 +1,23 @@
 #ifndef RACES_H_INCLUDED
 #define RACES_H_INCLUDED
+class Limb
+{
+public:
+    int StrModif;
+    int DexModif;
+}
+};
+class Arm : public Limb
+{
+    Arms* Weapon;
+};
+class Race
+{
+public:
+    float Blood;
+protected:
+    float MaxBlood;
+};
 
 
 
