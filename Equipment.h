0ed869eb8308b8873d9fc4f7eca75d8f1ff95dbd
Azorixz <koscielak.bartosz@gmail.com>
2014-04-13 01:31:58 +0200

    8

--- Equipment.h
+++ Equipment.h
@@ -1,7 +1,13 @@
 #ifndef EQUIPMENT_H_INCLUDED
 #define EQUIPMENT_H_INCLUDED
 
+class cWeapon
+{
+    int iPenetracja;
+    int iDmg;
+    int iAccuracyModyf;
 
+};
 
 
 #endif // EQUIPMENT_H_INCLUDED
--- Races.h
+++ Races.h
@@ -1,29 +1,40 @@
 #ifndef RACES_H_INCLUDED
 #define RACES_H_INCLUDED
+#include "Equipment.h"
 
-#include <vector>
 enum LIMBS {Limbs,Arms,Legs,Wings};
+
+class cLevel
+{
+
+
+};
+class cSkill
+{
+
+};
 
 class cLimb
 {
     public:
+        std::string sNazwa;
+        int iEfficiency
         int iStrModif;
         int iDexModif;
-
         int iNatPenetration;
-}
+        int iNatDmg;
 };
-class cArm : public Limb
+class cArm : public cLimb
 {
     public:
         cWeapon* Arms;
 };
-class cLeg : public Limb
+class cLeg : public cLimb
 {
     public:
         int iSpeed;
 };
-class cWing : public Limb
+class cWing : public cLimb
 {
     public:
         int iFlySpeed;
@@ -35,6 +46,8 @@ class cRace
         std::string sRaceName;
         unsigned uLimbsNumber[4];
         int iSize;
+//--------------------------
+        std::vector <cSkill*> vRaceSkills;
 
 
 };
@@ -43,16 +56,26 @@ class Creature
     public:
         std::string sName;
         cRace* Race;
+        std::vector <cLevel*> Levels
 //--------------------------
         int iStr;
         int iDex;
+        int iCon;
         int iInt;
         int iCha;
 //--------------------------
-
-
         float fBlood;
         float fMaxBlood;
+        float fEnergy;
+        float fMaxEnergy;
+//--------------------------
+        std::vector <cLimb> Limbs;
+        std::vector <cArm> Arms;
+        std::vector <cLeg> Legs;
+        std::vector <cWing> Wings;
+//---------------------------
+        Creature (std::string,cRace*);
+        Creature (std::string,cRace*,int,int,int,int);
 };
 
 #endif // RACES_H_INCLUDED
