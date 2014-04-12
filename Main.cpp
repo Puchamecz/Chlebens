a07235d8ba8451934336715e9f68b452dff7be6f
Puchamecz <mittorn@gmail.com>
2014-04-12 17:40:09 +0200

    6

---
+++ Main.cpp
@@ -0,0 +1,9 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+sf::RenderWindow window;
+
+int main ()
+{
+   return 0;
+}
--- Map.h
+++ Map.h
@@ -1,15 +1,30 @@
 #ifndef MAP_H_INCLUDED
 #define MAP_H_INCLUDED
 
-class MAPField
+#include <SFML/Graphics.hpp>
+
+class MapField
 {
-    int x;
-    int y;
+    sf::Vector3i position;
+    sf::Texture texture;
+    sf::Sprite sprite;
+    MapField (int c_x, int c_y, int c_z,)
+    {
+
+    }
 };
 
-class MAPObject
-{
+std::vector <MapField> surface;
 
+class MapObject
+{
+    sf::Vector3f position;
+    sf::Texture image;
+    sf::Sprite sprite;
+    MapObject (int c_x, int c_y, int c_z,)
+    {
+
+    }
 };
 
 #endif // MAP_H_INCLUDED
